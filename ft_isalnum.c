66e06ae10cc493327b6704ace9f960b837429835
alafa123 <nkinzingaboalafa@gmail.com>
2023-01-03 11:01:53 +0200

    libft

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: ankinzin <ankinzin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 12:16:45 by ankinzin          #+#    #+#             */
-/*   Updated: 2022/12/23 11:36:40 by ankinzin         ###   ########.fr       */
+/*   Updated: 2023/01/03 11:01:49 by ankinzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /**
  *	- The ft_isalnum() function tests for any character for which isalpha(3) or
  *	isdigit(3) is true.  The value of the argument must be representable as
- *	an unsigned char or the value of EOF.
+ *	an unsigned char or the value of EOF .
  *
  *	- The isalnum() function returns zero if the character tests false
  *	and returns non-zero if the character tests true.
